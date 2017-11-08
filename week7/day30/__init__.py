4ca8a8b9f4d412fa3477213007736cf9429ebba5
wangchongsheng <wang_chongsheng@163.com>
2017-11-08 18:06:27 +0800

    20171106

---
+++ week7/day30/__init__.py
@@ -0,0 +1,4 @@
+# @Time    : 2017/11/8 0008 18:01
+# @Author  : "Wang_Chongsheng"
+# @Site    : 
+# @File    : __init__.py.py
---
+++ week7/day30/test.py
@@ -0,0 +1,7 @@
+# @Time    : 2017/11/8 0008 18:04
+# @Author  : "Wang_Chongsheng"
+# @Site    : 
+# @File    : test.py
+
+class Foo:
+    pass
